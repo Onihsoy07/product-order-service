b9cc97c693bc9cf43c7aefd7f0ddf90d292d0d2b
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-01 23:56:11 +0900

    테스트 : Item 저장 API

---
+++ src/test/java/com/example/productorderservice/service/ProductApiTest.java
@@ -0,0 +1,38 @@
+package com.example.productorderservice.service;
+
+import com.example.productorderservice.config.ApiTestConfig;
+import com.example.productorderservice.domain.dto.ItemSaveDto;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class ProductApiTest extends ApiTestConfig {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 상품등록() {
+        final ItemSaveDto itemSaveDto = new ItemSaveDto("상품명", 9999, "none");
+
+        //API 요청
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(itemSaveDto)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+}
