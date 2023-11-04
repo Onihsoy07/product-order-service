a5347658737323d1d8fc27a7225a8ad58a5dd396
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-04 23:28:56 +0900

    테스트 : Item 조회

--- src/test/java/com/example/productorderservice/service/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/service/ProductServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.productorderservice.service;
 
+import com.example.productorderservice.domain.dto.ItemDto;
 import com.example.productorderservice.domain.dto.ItemSaveDto;
+import com.example.productorderservice.domain.enumeration.DiscountPolicy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -18,5 +23,27 @@ public class ProductServiceTest {
         ItemSaveDto itemSaveDto = new ItemSaveDto("상품명", 9999, "none");
 
         productService.addItem(itemSaveDto);
+    }
+
+    @Test
+    void 상품조회() {
+        //given
+        String itemName = "상품명";
+        int price = 9999;
+        DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+//        ItemSaveDto itemSaveDto = new ItemSaveDto(itemName, price, discountPolicy.getValue());
+//        productService.addItem(itemSaveDto);
+        final long itemId = 1L;
+
+        //when
+        ItemDto itemDto = productService.getItem(itemId);
+
+        //then
+        assertThat(itemDto).isNotNull();
+        assertThat(itemDto.getItemName()).isEqualTo(itemName);
+        assertThat(itemDto.getPrice()).isEqualTo(price);
+        assertThat(itemDto.getDiscountPolicy()).isEqualTo(discountPolicy.getValue());
+
+
     }
 }
