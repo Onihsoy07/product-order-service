2358be77b8fa7b28be0a9c4f4bb2ebf0a66dc0e0
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-11-04 23:39:47 +0900

    구현 : Item 조회 API

--- src/main/java/com/example/productorderservice/controller/ProductApiController.java
+++ src/main/java/com/example/productorderservice/controller/ProductApiController.java
@@ -1,15 +1,13 @@
 package com.example.productorderservice.controller;
 
+import com.example.productorderservice.domain.dto.ItemDto;
 import com.example.productorderservice.domain.dto.ItemSaveDto;
 import com.example.productorderservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,6 +24,13 @@ public class ProductApiController {
         productService.addItem(itemSaveDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemDto> getItem(@PathVariable("itemId") Long itemId) {
+        ItemDto itemDto = productService.getItem(itemId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(itemDto);
     }
 
 
